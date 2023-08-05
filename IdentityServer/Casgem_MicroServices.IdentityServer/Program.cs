6f175f0f6560a50b0e293acf848669ca635b0d12
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-05 11:42:51 +0300

    add-migration mig1 update-database komutları ile DB güncellendi.

--- IdentityServer/Casgem_MicroServices.IdentityServer/Program.cs
+++ IdentityServer/Casgem_MicroServices.IdentityServer/Program.cs
@@ -48,32 +48,33 @@ namespace Casgem_MicroServices.IdentityServer
                 }
 
                 var host = CreateHostBuilder(args).Build();
-                using (var scope = host.Services.CreateScope()) {
+                using (var scope = host.Services.CreateScope())
+                {
                     var serviceProvider = scope.ServiceProvider;
                     var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
                     applicationDbContext.Database.Migrate();
                     var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
                     if (!userManager.Users.Any())
                     {
-                        userManager.CreateAsync(new ApplicationUser{
+                        userManager.CreateAsync(new ApplicationUser
+                        {
                             UserName = "huseyinaydin99",
                             Email = "huseyinaydin99@gmail.com",
                             City = "Niğde",
                             Country = "Turkiye",
                             NameSurname = "Hüseyin Aydın"
                         }, "123456Aa*");
-
                     }
                 };
-                    if (seed)
-                    {
-                        Log.Information("Seeding database...");
-                        var config = host.Services.GetRequiredService<IConfiguration>();
-                        var connectionString = config.GetConnectionString("DefaultConnection");
-                        SeedData.EnsureSeedData(connectionString);
-                        Log.Information("Done seeding database.");
-                        return 0;
-                    }
+                if (seed)
+                {
+                    Log.Information("Seeding database...");
+                    var config = host.Services.GetRequiredService<IConfiguration>();
+                    var connectionString = config.GetConnectionString("DefaultConnection");
+                    SeedData.EnsureSeedData(connectionString);
+                    Log.Information("Done seeding database.");
+                    return 0;
+                }
 
                 Log.Information("Starting host...");
                 host.Run();
