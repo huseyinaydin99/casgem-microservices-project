5725c36cb9ce111400ac1a859c9db813f90d3234
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-05 14:53:34 +0300

    Config.cs dosyası büyük oranda yazıldı.

--- IdentityServer/Casgem_MicroServices.IdentityServer/Config.cs
+++ IdentityServer/Casgem_MicroServices.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Casgem_MicroServices.IdentityServer
@@ -106,31 +107,65 @@ namespace Casgem_MicroServices.IdentityServer
             new Client[]
             {
                 // m2m client credentials flow client
-                new Client
+                new Client//sisteme giriş yaparken üye olmak zorunda olmayan!!! istemci.
                 {
+                    ClientName = "Casgem1",
+                    ClientId = "CoreClient1",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes =
+                    {
+                        "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName
+                    }
+                    /*
                     ClientId = "m2m.client",
                     ClientName = "Client Credentials Client",
-
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
                     AllowedScopes = { "scope1" }
+                    */
                 },
 
                 // interactive client using code flow + pkce
-                new Client
+                new Client //sisteme giriş yaparken üye olmak zorunda olan istemci.
                 {
+                    ClientName = "Casgem2",
+                    ClientId = "CoreClient2",
+                    AllowOfflineAccess = true,
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = 
+                    {
+                        "basket_fullpermission",
+                        "order_fullpermission",
+                        "discount_fullpermission",
+                        "cargo_fullpermission",
+                        "payment_fullpermission",
+                        "photo_stock_fullpermission",
+                        "catalog_fullpermission", 
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        IdentityServerConstants.LocalApi.ScopeName, "roles"
+                    },
+                    AccessTokenLifetime = 1*60*60,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int) (DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage = TokenUsage.ReUse
+                    /*
                     ClientId = "interactive",
                     ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
                     AllowedGrantTypes = GrantTypes.Code,
-
                     RedirectUris = { "https://localhost:44300/signin-oidc" },
                     FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
                     PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
+                    */
                 },
             };
     }
