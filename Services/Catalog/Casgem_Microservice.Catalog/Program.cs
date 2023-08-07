ef0da8549fd8f303e669e220c511fb5416b972c8
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-07 12:20:47 +0300

    launchsettings ayarları yapıldı. IdentityServer ve Catalog servisleri aynı anda başlatıldı. IdentityServer program.cs, config.cs, startup.cs ayarları yapıldı.

--- Services/Catalog/Casgem_Microservice.Catalog/Program.cs
+++ Services/Catalog/Casgem_Microservice.Catalog/Program.cs
@@ -2,6 +2,8 @@ using Casgem_Microservice.Catalog.Services.CategoryServices;
 using Casgem_Microservice.Catalog.Services.ProductServices;
 using Casgem_Microservice.Catalog.Settings.Abstracts;
 using Casgem_Microservice.Catalog.Settings.Concretes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,13 +19,25 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 {
     return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
 });
 
-var app = builder.Build();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerURL"];
+    opt.Audience = "resource_catalog";
+    opt.RequireHttpsMetadata = false;
+});
 
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -33,6 +47,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
